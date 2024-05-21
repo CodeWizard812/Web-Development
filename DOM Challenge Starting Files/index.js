92e44baba86febea8542559b654a859b4ffcc70e
CodeWizard812 <shirish.professional@gmail.com>
2024-05-21 16:08:47 +0530

    Day 10

---
+++ DOM Challenge Starting Files/index.js
@@ -0,0 +1,7 @@
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Shirish";
+
+document.querySelector(".list > a").style.color = "Red" ;
+
+document.querySelector("button").style.backgroundColor = "Yellow";
+
+document.querySelector("h1").classList.add("huge");
