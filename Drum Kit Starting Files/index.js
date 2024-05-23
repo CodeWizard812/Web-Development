8752a6ee56e0d985d8eba398dd4d6f5499fffa62
CodeWizard812 <shirish.professional@gmail.com>
2024-05-23 12:50:14 +0530

    Day 13

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,41 @@
+for  (var i = 0; i < document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+        this.style.color = "white";
+        setTimeout(function(){
+            this.style.color = "#DA0463";  // Change the color back to the original
+          }, 1000);
+        switch (this.innerHTML) {
+            case "w":
+                var a = new Audio('./sounds/tom-1.mp3');
+                a.play();
+                break;
+            case "a":
+                var a = new Audio('./sounds/tom-2.mp3');
+                a.play();
+                break;
+            case "s":
+                var a = new Audio('./sounds/tom-3.mp3');
+                a.play();
+                break;
+            case "d":
+                var a = new Audio('./sounds/tom-4.mp3');
+                a.play();
+                break;
+            case "j":
+                var a = new Audio('./sounds/snare.mp3');
+                a.play();
+                break;
+            case "k":
+                var a = new Audio('./sounds/crash.mp3');
+                a.play();
+                break;
+            case "l":
+                var a = new Audio('./sounds/kick-bass.mp3');
+                a.play();
+                break;
+            default:
+                console.log(this.innerHTML);
+                break;
+        }
+    });
+}
